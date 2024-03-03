309f65213c0cce89c56870a7555eeaa3d93ee31e
Evelyn Hernandez <evelynbhr00@gmail.com>
2024-03-03 16:29:12 -0600

    fix: corregir botón de scartter plot

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-st.header('Vehicles us')
+st.header('Vehicles US')
 
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 hist_button = st.button('Construir histograma') # crear un botón
@@ -18,7 +18,7 @@ if hist_button: # al hacer clic en el botón
     st.plotly_chart(fig, use_container_width=True)
 
 
-hist_button = st.button('Construir gráfico de dispersión') # crear un botón
+scatter_button = st.button('Construir gráfico de dispersión') # crear un botón
 
 if scatter_button: #al hacer clic en el botón
 #escribir mensaje
@@ -27,5 +27,4 @@ if scatter_button: #al hacer clic en el botón
     
     #crear un scartter plot
     fig = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
-    fig.show() # crear gráfico de dispersión
-
+    st.plotly_chart(fig, use_container_width=True) # crear gráfico de dispersión
